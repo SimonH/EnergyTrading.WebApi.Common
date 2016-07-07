9649168cd3dc4c3fc972c61895124b1006784cc6
simonh <simonh@DATPCDI013>
2016-07-07 12:32:11 +0100

    Added interface and exception type for use in conjunction with WebapiRestServer project

---
+++ EnergyTrading.WebApi.Common/Faults/FaultException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnergyTrading.WebApi.Common.Faults
+{
+    public class FaultException : Exception
+    {
+        public FaultException(Fault fault) : base(fault.ErrorMessage)
+        {
+            Fault = fault;
+        }
+
+        public Fault Fault { get; private set; }
+    }
+}
---
+++ EnergyTrading.WebApi.Common/Server/ICreateLoggerFactories.cs
@@ -0,0 +1,13 @@
+﻿using EnergyTrading.Logging;
+
+namespace EnergyTrading.WebApi.Common.Server
+{
+    public interface ICreateLoggerFactories
+    {
+        /// <summary>
+        /// Creates an instance of ILoggerFactory or throws a FaultException containing the error information
+        /// </summary>
+        /// <returns>The created ILoggerFactory</returns>
+        ILoggerFactory CreateInstance();
+    }
+}
