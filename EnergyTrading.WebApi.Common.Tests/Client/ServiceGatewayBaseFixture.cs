ed5fd56169c9789f2721650a263d58d46285d830
SimonH <Simon@slimtech-it.co.uk>
2016-09-20 15:42:51 +0100

    Added ServiceGatewayBase for a single path on a rest service uri Added tests updated nuspec version

---
+++ EnergyTrading.WebApi.Common.Tests/Client/ServiceGatewayBaseFixture.cs
@@ -0,0 +1,36 @@
+﻿using EnergyTrading.WebApi.Common.Client;
+using NUnit.Framework;
+
+namespace EnergyTrading.WebApi.Common.Tests.Client
+{
+    [TestFixture]
+    public class ServiceGatewayBaseFixture
+    {
+        private class TestServiceGateway : ServiceGatewayBase
+        {
+            public TestServiceGateway(string baseUri, string path) : base(baseUri, path)
+            {
+            }
+
+            public string ExposedServiceUri => ServiceUri;
+        }
+
+        [Test]
+        [TestCase("http://basevalue", "", "http://basevalue")]
+        [TestCase("http://basevalue", null, "http://basevalue")]
+        [TestCase("http://basevalue", "   ", "http://basevalue")]
+        [TestCase("http://basevalue/", "", "http://basevalue")]
+        [TestCase("http://basevalue/", null, "http://basevalue")]
+        [TestCase("http://basevalue/", "   ", "http://basevalue")]
+        [TestCase("http://basevalue", "/", "http://basevalue")]
+        [TestCase("http://basevalue/", "/", "http://basevalue")]
+        [TestCase("http://basevalue", "/some/path/value", "http://basevalue/some/path/value")]
+        [TestCase("http://basevalue/", "/some/path/value", "http://basevalue/some/path/value")]
+        [TestCase("http://basevalue", "some/path/value", "http://basevalue/some/path/value")]
+        [TestCase("http://basevalue/", "some/path/value", "http://basevalue/some/path/value")]
+        public void ConstructedServiceUri(string baseUri, string path, string expected)
+        {
+            Assert.That(new TestServiceGateway(baseUri, path).ExposedServiceUri, Is.EqualTo(expected));
+        } 
+    }
+}
---
+++ EnergyTrading.WebApi.Common/Client/ServiceGatewayBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnergyTrading.WebApi.Common.Client
+{
+    public abstract class ServiceGatewayBase : RestGatewayBase
+    {
+        protected ServiceGatewayBase(string baseUri, string path)
+        {
+            if (string.IsNullOrWhiteSpace(baseUri))
+            {
+                throw new ArgumentOutOfRangeException(nameof(baseUri));
+            }
+            new Uri(baseUri); // tests that baseUri is a valid uri string
+            // conformity strip of trailing / here and only add it if there is a path
+            this.ServiceUri = baseUri.EndsWith("/") ? baseUri.Substring(0, baseUri.Length -1) : baseUri;
+            if (path != null)
+            {
+                var withoutSlash = path.StartsWith("/") ? path.Substring(1) : path;
+                if (!string.IsNullOrWhiteSpace(withoutSlash))
+                {
+                    ServiceUri += "/" + withoutSlash;
+                }
+            }
+        }
+
+        protected string ServiceUri { get; }
+    }
+}
