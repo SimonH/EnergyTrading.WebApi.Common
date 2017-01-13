624eb7d3661a132f76dea98fa1a648b7b89a3a3c
SimonH <Simon@slimtech-it.co.uk>
2017-01-13 11:44:55 +0000

    Added ContractReference and RelatedContractReference raised version in nuspec

---
+++ EnergyTrading.WebApi.Common/Contracts/ContractReference.cs
@@ -0,0 +1,7 @@
+﻿namespace EnergyTrading.WebApi.Common.Contracts
+{
+    public class ContractReference : MappedContract
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ EnergyTrading.WebApi.Common/Contracts/RelatedContractReference.cs
@@ -0,0 +1,8 @@
+﻿namespace EnergyTrading.WebApi.Common.Contracts
+{
+    public class RelatedContractReference
+    {
+        public EnumValueType Relationship { get; set; }
+        public ContractReference Contract { get; set; }
+    }
+}
